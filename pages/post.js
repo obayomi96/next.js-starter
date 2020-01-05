337f4d38e81158552d1baf4e287457d330bc7de5
martins obayomi <obayomimartins96@gmail.com>
2020-01-05 17:24:44 +0100

    view commnets on a single post

--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,26 @@
-const Post = ({ id }) => {
-  return (
-  <div>YOUR ARE LOOKIGN AT POST #{id}</div>
-  );
-}
+import axios from 'axios';
 
-Post.getInitialProps = async ({ query }) => {
-  return query;
+const Post = ({ id, comments }) => (
+  <div>
+    <h1>COMMENTS AT POST #{id}</h1>
+    {comments.map(comment => (
+      <Comment {...comment} key={comment.id} />
+     ))}
+  </div>
+  );
+
+  const Comment = ({email, body}) => (
+    <div>
+      <h5>{email}</h5>
+      <p>{body}</p>
+    </div>
+  );
+
+Post.getInitialProps = async ({ query }) => {const res = await axios.get(`https://jsonplaceholder.typicode.com/comments?postId=${query.id}`);
+const { data } = res;
+
+console.log('data', data[0]);
+return { ...query, comments: data };
 };
 
 export default Post;
